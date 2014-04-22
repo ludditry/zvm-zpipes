a915687e370c7713bb0e2b6e1e65979abeeca079
rpedde <ron@pedde.com>
2014-04-22 11:05:04 -0500

    update to newest zpipes

--- src/zvm_zpipes.c
+++ src/zvm_zpipes.c
@@ -12,6 +12,15 @@ typedef struct pipe_info_t {
     zpipes_client_t *pclient;
 } pipe_info_t;
 
+
+__attribute__((constructor)) void init(void);
+
+void
+init(void)
+{
+    zsys_handler_set(NULL);
+}
+
 ZVMPIPE 
 zvm_pipe_open(const char *descriptor)
 {
@@ -54,13 +63,26 @@ zvm_pipe_close(ZVMPIPE pipe)
 ssize_t
 zvm_pipe_read(ZVMPIPE pipe, void *buf, size_t count)
 {
-    return zpipes_client_read(pipe->pclient, buf, count);
+    ssize_t size;
+
+    size = zpipes_client_read(pipe->pclient, buf, count, 0);
+
+    if (size < 0) 
+        errno = zpipes_client_error(pipe->pclient);
+
+    return size;
 }
 
 ssize_t
 zvm_pipe_write(ZVMPIPE pipe, void *buf, size_t count)
 {
-    zpipes_client_write(pipe->pclient, buf, count);
-    return count;
+    int size;
+
+    size = zpipes_client_write(pipe->pclient, buf, count, 0);
+
+    if (size < 0)
+        errno = zpipes_client_error(pipe->pclient);
+
+    return size;
 }
 
